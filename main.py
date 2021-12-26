cfc6662001b3fe06362843630e01714e0aafd896
svidrig_cth <svidrig_cth@Acer.svidrig_cth.com>
2021-12-26 05:40:21 +0300

    add example

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ async def img(name_dir, url_img):
         try:
             async with session.get(url) as resp:
                 if resp.status == 200:
-                    f = await aiofiles.open(str(os.path.dirname(__file__))+'/image'+dir+'/'+url.split('/')[-1], mode='wb')
+                    f = await aiofiles.open(str(os.path.dirname(__file__))+'/image/'+dir+'/'+url.split('/')[-1], mode='wb')
                     await f.write(await resp.read())
                     await f.close()
                 else:
@@ -33,8 +33,6 @@ async def get_url_data(name_dir, urls):
 def start_parse(name_dir, urls):
     global last_page
     dir = name_dir
-    last_page = int(urls[-1].split('/')[-1].split('.')[-2])
-    print('Всего в деле:', str(last_page))
     loop = asyncio.get_event_loop()
     loop.run_until_complete(get_url_data(dir, urls))
 
@@ -43,13 +41,13 @@ def parse_image(name_dir, download_err=False, parse_error=[]):
     dir = name_dir
     path = str(os.path.dirname(__file__))
     try:
-        os.makedirs(path+'/image'+dir)
-        print("Папка создана:", path+'/image'+dir)
+        os.makedirs(path+'/image/'+dir)
+        print("Папка создана:", path+'/image/'+dir)
     except Exception:
-        print("Папка существует:", path+'/image'+dir)
+        print("Папка существует:", path+'/image/'+dir)
     
     if download_err == False:
-        urls = open(path+'/text'+dir+'.txt', 'r').read().split('\n')
+        urls = open(path+'/text/'+dir+'.txt', 'r').read().split('\n')
         start_parse(dir, urls)
     
     elif len(parse_error) >= 1:
@@ -64,7 +62,7 @@ def parse_image(name_dir, download_err=False, parse_error=[]):
 
 def main(dir):
     global arr_err
-    print('Начинаю работу с проектом ' + dir[1:])
+    print('Начинаю работу с проектом ' + dir)
     parse_image(name_dir=dir)
     j = 0
     print('Всего ошибочных:', len(arr_err))
@@ -78,15 +76,7 @@ def main(dir):
 
 if __name__ == "__main__":
     dirs = [
-        '/93',
-        '/95',
-        '/102',
-        '/112',
-        '/127',
-        '/136',
-        '/137',
-        '/153',
-        '/154'
+        'test'
     ]
     for dir in dirs:
         main(dir)
